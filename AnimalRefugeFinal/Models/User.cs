0698321e871d43d02a313a1af0778b2aa6023b3f
jeffjack38 <jeaden38@gmail.com>
2023-10-11 18:58:16 -0500

    Added Models

---
+++ AnimalRefugeFinal/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimalRefugeFinal.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } // "Admin" or "User"
+
+        // Navigation property for favorites
+        public ICollection<Favorite> Favorites { get; set; }
+
+        // Navigation property for adoption applications
+        public ICollection<AdoptionApplication> AdoptionApplications { get; set; }
+    }
+}
