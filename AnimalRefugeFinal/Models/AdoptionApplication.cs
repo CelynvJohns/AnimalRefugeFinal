f6375562b234a44fd6f65ebecb29af4e197a2282
Celyn Johns <143140229+CelynvJohns@users.noreply.github.com>
2023-11-29 20:40:01 -0600

    Added in the questions needed for the Adoption Application

--- AnimalRefugeFinal/Models/AdoptionApplication.cs
+++ AnimalRefugeFinal/Models/AdoptionApplication.cs
@@ -32,7 +32,12 @@ namespace AnimalRefugeFinal.Models
 
         public Pet[] Pets { get; set; }
 
-        public string CurrentPets { get; set; }
+        public CurrentPet[] CurrentPet { get; set; } // Corrected property name to CurrentPets
+        public string CurrentPetName { get; set; }
+        public int CurrentPetAge { get; set; }
+        public int CurrentPetBreed { get; set; }
+        public string VetName { get; set; }
+        public int VetPhoneNumber { get; set; }
 
         [Required]
         public string Reason { get; set; }
@@ -41,5 +46,49 @@ namespace AnimalRefugeFinal.Models
 
         public int StatusId { get; set; }
         public string Status { get; set; } // "Pending", "Approved", "Rejected"
+
+        public CurrentHumans[] CurrentHumans { get; set; } // Corrected property name to Humans
+        public string CurrentHumanFirstName { get; set; }
+        public string CurrentHumanLastName { get; set; }
+        public string CurrentHumanAge { get; set; }
+
+        public int CurrentPetNumber { get; set; }
+        public int CurrentHumansNumber { get; set; }
+
+        public bool WantsToAdoptDog { get; set; }
+
+        public bool HasFencedInYard { get; set; }
+    }
+
+    public class CurrentHumans
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; } 
+    }
+
+    public class CurrentPet
+    {
+        [Required]
+        public string CurrentPetName { get; set; }
+
+        [Required]
+        public int CurrentPetAge { get; set; }
+
+        [Required]
+        public int CurrentPetBreed { get; set; }
+
+        [Required]
+        public string VetName { get; set; }
+
+        [Required]
+        public int VetPhoneNumber { get; set; }
+
+
     }
 }
