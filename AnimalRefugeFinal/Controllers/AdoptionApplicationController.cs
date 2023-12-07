657e49fc0371e91f7da4763b38ded6cdd82c865c
Jeff "Jack" Aden <jeaden38@gmail.com>
2023-12-06 21:11:11 -0600

    Removed adoption action from UserController and placed them into AdoptionApplicationController
    Removed adoption action from UserController and placed them into AdoptionApplicationController

--- AnimalRefugeFinal/Controllers/AdoptionApplicationController.cs
+++ AnimalRefugeFinal/Controllers/AdoptionApplicationController.cs
@@ -1,23 +1,64 @@
-﻿using AnimalRefugeFinal.Models;
+﻿using AnimalRefugeFinal.Models.AnimalRefugeFinal.Models;
+using AnimalRefugeFinal.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
 
 namespace AnimalRefugeFinal.Controllers
 {
-    public class AdoptionApplicationController : Controller
+    [Authorize] // Use this attribute if only authenticated users can apply for adoption
+    public class AdoptionController : Controller
     {
-        public IActionResult AdoptionApplication()
-        {
-            // Create an instance of AdoptionApplication model
-            var model = new AnimalRefugeFinal.Models.AdoptionApplication();
+        private readonly PetContext _context;
 
-            // Check if the user is authenticated
-            if (User.Identity.IsAuthenticated)
+        public AdoptionController(PetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ApplyForAdoption(int petId)
+        {
+            // Assuming you have a view model for the adoption application form
+            var viewModel = new AdoptionApplicationViewModel
             {
-                // Set the username in the model
-                model.User = new User { Username = User.Identity.Name };
+                PetId = petId
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ApplyForAdoption(AdoptionApplicationViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Model validation failed, return to the adoption application form with validation errors
+                return View(viewModel);
             }
 
-            return View(model);
+            // Get the current user's ID
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userId = int.Parse(userIdString);
+
+            // Create a new AdoptionApplication based on the submitted data
+            var application = new AdoptionApplication
+            {
+                UserId = userId,
+                PetId = viewModel.PetId,
+                Reasons = viewModel.Reasons,
+                ApplicationDate = DateTime.Now,
+                Status = "Pending" // Set the initial status
+            };
+
+            // Add the adoption application to the database
+            _context.AdoptionApplications.Add(application);
+            _context.SaveChanges();
+
+            // Redirect to a confirmation page or the pet details page
+            return RedirectToAction("Details", "Pet", new { id = viewModel.PetId });
         }
     }
 }
+
