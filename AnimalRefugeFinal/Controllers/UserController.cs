045ee1b6697c4921e86899c1ce4020c8049df131
Jeff "Jack" Aden <jeaden38@gmail.com>
2023-12-06 21:00:18 -0600

    update to EditUser
    update to EditUser

--- AnimalRefugeFinal/Controllers/UserController.cs
+++ AnimalRefugeFinal/Controllers/UserController.cs
@@ -50,7 +50,31 @@ namespace AnimalRefugeFinal.Controllers
         // Display a form for editing the user's profile
         // Handle POST request to update the user's profile
         [HttpGet]
-        public IActionResult EditProfile(User updatedUser)
+        public IActionResult EditProfile()
+        {
+            
+                // Get the current user's ID
+                var userIdString = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var userId = int.Parse(userIdString);
+
+                // Fetch the user's profile information
+                var userProfile = _context.Users
+                    .Where(user => user.Id == userId)
+                    .FirstOrDefault();
+
+                if (userProfile != null)
+                {
+                    return View(userProfile);
+                }
+
+                // If the user profile is not found, you can handle it accordingly (e.g., redirect to an error page)
+                return NotFound();
+            
+
+        }
+
+        [HttpPost]
+        public IActionResult EditProfile(User editedUser)
         {
             if (ModelState.IsValid)
             {
@@ -63,36 +87,13 @@ namespace AnimalRefugeFinal.Controllers
                     .Where(user => user.Id == userId)
                     .FirstOrDefault();
 
-                if (userProfile != null)
-                {
-                    // Update the user's profile information
-                    userProfile.Username = updatedUser.Username;
-                    userProfile.PasswordHash = updatedUser.PasswordHash;
-                    userProfile.FirstName = updatedUser.FirstName;
-                    userProfile.LastName = updatedUser.LastName;
+                // Update the user's profile information
+                userProfile.Username = editedUser.Username;
+                userProfile.PasswordHash = editedUser.PasswordHash;
+                userProfile.FirstName = editedUser.FirstName;
+                userProfile.LastName = editedUser.LastName;
 
-                    _context.SaveChanges();
 
-                    // Redirect to the profile page after updating
-                    return RedirectToAction("Profile");
-                }
-            }
-
-            // If the model state is not valid, return to the edit profile page with validation errors
-            return View(updatedUser);
-        }
-
-        [HttpPost]
-        public IActionResult EditProfile(User editedUser)
-        {
-            if (ModelState.IsValid)
-            {
-                // Add logic to update the user's profile in the database
-                var user = _context.Users.Find(editedUser.Id);
-                user.Username = editedUser.Username;
-                user.PasswordHash = editedUser.PasswordHash;
-                user.FirstName = editedUser.FirstName;
-                user.LastName = editedUser.LastName;
                 _context.SaveChanges();
 
                 // Redirect to the user's profile after successful profile update
