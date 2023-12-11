9c91c4fe1082e51a64e03171e97ee5d23723b4ec
Celyn Johns <143140229+CelynvJohns@users.noreply.github.com>
2023-12-10 20:38:32 -0600

    Filter Works

--- AnimalRefugeFinal/Controllers/PetController.cs
+++ AnimalRefugeFinal/Controllers/PetController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AnimalRefugeFinal.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalRefugeFinal.Controllers
 {
@@ -51,6 +52,48 @@ namespace AnimalRefugeFinal.Controllers
             // Pass the filtered pets to the view
             return View("ViewList", filteredPets);
         }
+
+        // FilterByAge Action
+        // Filter by age range
+        public IActionResult FilterByAge(string ageRange)
+        {
+            // Parse the selected age range
+            var ageBounds = ageRange.Split('-').Select(int.Parse).ToArray();
+
+            // Filter pets by age range
+            var filteredPets = _context.Pets.Where(p => p.Age >= ageBounds[0] && p.Age <= ageBounds[1]).ToList();
+
+            // Pass the filtered pets to the view
+            return View("ViewList", filteredPets);
+        }
+
+
+        // Filter Action
+        // Filter pets by species, age, or both
+        public IActionResult Filter(string species, string ageRange)
+        {
+            IQueryable<Pet> filteredPets = _context.Pets;
+
+            // Filter by species
+            if (!string.IsNullOrEmpty(species))
+            {
+                filteredPets = filteredPets.Where(p => p.Species == species);
+            }
+
+            // Filter by age range
+            if (!string.IsNullOrEmpty(ageRange))
+            {
+                // Parse the selected age range
+                var ageBounds = ageRange.Split('-').Select(int.Parse).ToArray();
+
+                // Filter pets by age range
+                filteredPets = filteredPets.Where(p => p.Age >= ageBounds[0] && p.Age <= ageBounds[1]);
+            }
+
+            // Pass the filtered pets to the view
+            return View("ViewList", filteredPets.ToList());
+        }
     }
+
 }
 
