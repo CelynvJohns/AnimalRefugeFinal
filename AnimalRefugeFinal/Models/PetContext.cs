e1188d7c9361f097eeae37dd13cca7a778d6c8e7
Celyn Johns <143140229+CelynvJohns@users.noreply.github.com>
2023-12-10 22:07:51 -0600

    20 pets total in db, need to push it to db though

--- AnimalRefugeFinal/Models/PetContext.cs
+++ AnimalRefugeFinal/Models/PetContext.cs
@@ -22,7 +22,22 @@ namespace AnimalRefugeFinal.Models
 
             modelBuilder.Entity<Pet>().HasData(
                 new Pet { Id = 1, Name = "Fluffy", Species = "Cat", Age = 2, Description = "A cute fluffy cat.", BondedBuddyStatus = "None", SpecialCareInstructions = "Needs exercise" },
-                new Pet { Id = 2, Name = "Buddy", Species = "Dog", Age = 3, Description = "A friendly dog looking for a home.", BondedBuddyStatus = "None", SpecialCareInstructions = "Is diabetic"}
+                new Pet { Id = 2, Name = "Buddy", Species = "Dog", Age = 3, Description = "A friendly dog looking for a home.", BondedBuddyStatus = "None", SpecialCareInstructions = "Is diabetic"},
+                // pets 3-8 murdered :( (they're still in the database but not here, it's a okay tho)
+                // These need to be ADDED to the DB
+                 new Pet { Id = 9, Name = "Raven", Species = "Cat", Age = 2, Description = "enchants with her silky midnight-blue fur and eyes that gleam like twin crescent moons, exuding an aura of serene elegance and quiet wisdom.", BondedBuddyStatus = "None", SpecialCareInstructions = "Needs attention" },
+                new Pet { Id = 10, Name = "Worm", Species = "Dog", Age = 3, Description = "a gentle soul, displaying a heartwarming affection by leaning into every pat and responding to commands with a calm and eager demeanor.", BondedBuddyStatus = "None", SpecialCareInstructions = "Afraid of worms" },
+                 new Pet { Id = 11, Name = "Jack", Species = "Cat", Age = 2, Description = "a calm observer, finding solace in quiet corners and displaying a gentle, affectionate nature by curling up in laps during cozy evenings.", BondedBuddyStatus = "None", SpecialCareInstructions = "Needs litter to be extra clean" },
+                new Pet { Id = 12, Name = "Inu", Species = "Dog", Age = 3, Description = "a lively bundle of joy, his tail in a perpetual state of enthusiastic wagging as he greets everyone with boundless energy.", BondedBuddyStatus = "None", SpecialCareInstructions = "Is diabetic" },
+                 new Pet { Id = 13, Name = "Yoshi", Species = "Reptile", Age = 22, Description = "a patient and resilient friend, gracefully gliding through the water with a quiet determination and occasionally retreating into her shell to reflect", BondedBuddyStatus = "Daisey", SpecialCareInstructions = "Needs alone time" },
+                new Pet { Id = 14, Name = "Touka", Species = "Reptile", Age = 39, Description = "embodies an enigmatic grace, sinuously navigating her surroundings with hypnotic movements.", BondedBuddyStatus = "None", SpecialCareInstructions = "Good with kids" },
+                 new Pet { Id = 15, Name = "Meryl", Species = "Reptile", Age = 20, Description = "moves through life with a calm and deliberate pace, his shell adorned with the wisdom of ages ", BondedBuddyStatus = "None", SpecialCareInstructions = "Good with other pets" },
+                new Pet { Id = 16, Name = "Patches", Species = "Bunny", Age = 3, Description = "a curious and nimble explorer, darting around with playful hops.", BondedBuddyStatus = "Dusty", SpecialCareInstructions = "Is skitish" },
+                 new Pet { Id = 17, Name = "Dusty", Species = "Bunny", Age = 5, Description = "a mischievous delight, showcasing a penchant for binkying through open spaces, executing playful jumps with contagious excitement", BondedBuddyStatus = "Patches", SpecialCareInstructions = "Needs extra love" },
+                new Pet { Id = 18, Name = "Pixie", Species = "Bunny", Age = 13, Description = "a serene and gentle companion, preferring quiet moments of nibbling on fresh greens and lounging in sunlit spots.", BondedBuddyStatus = "None", SpecialCareInstructions = "Needs to be the only pet" },
+                 new Pet { Id = 19, Name = "Destiny", Species = "Cat", Age = 4, Description = "Mystique, the cat, boasts a sleek onyx coat and captivating green eyes, exuding an air of graceful mystery and feline elegance.", BondedBuddyStatus = "None", SpecialCareInstructions = "None" },
+                new Pet { Id = 20, Name = "Chilli", Species = "Dog", Age = 6, Description = "an adventurous spirit, always ready for a new escapade, whether it's exploring the outdoors or chasing after frisbees.", BondedBuddyStatus = "None", SpecialCareInstructions = "None" }
+
                 );
 
             //Other configurations or seed data go here
